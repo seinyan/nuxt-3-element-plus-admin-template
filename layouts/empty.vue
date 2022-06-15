9174f94f5636dff07fcff206482f1f3978328794
Seinyan <narek.seinyan@gmail.com>
2022-06-15 21:15:49 +0400

    commit 2

--- layouts/empty.vue
+++ layouts/empty.vue
@@ -1,7 +1,3 @@
 <template>
-  <div class="min-h-100-vh flex-center" v-loading="$store.state.loading">
-    <transition name="fade-transform" mode="out-in">
-      <nuxt/>
-    </transition>
-  </div>
+  <slot/>
 </template>
